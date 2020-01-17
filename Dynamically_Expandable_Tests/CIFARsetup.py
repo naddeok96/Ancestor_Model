7e464e890c6d742c64e858b6da4748f87595da9f
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-01-16 23:49:20 -0800

    adding parameter freezing

--- Dynamically_Expandable_Tests/CIFARsetup.py
+++ Dynamically_Expandable_Tests/CIFARsetup.py
@@ -119,7 +119,7 @@ class CIFAR10_Setup:
                 '''
 
                 print(self.net.conv1.weight.grad)
-                self.net.conv1.weight.grad[0:40, :, :] = 0
+                self.net.conv1.weight.grad[0:2, :, :] = 0
                 print(np.size(self.net.conv1.weight.grad))
 
                 print("Stop Here \n")
