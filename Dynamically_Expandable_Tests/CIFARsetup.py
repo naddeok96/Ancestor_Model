9dfd829c455922a91469e32efac3e85c0793abb1
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-01-17 16:33:31 -0800

    adding parameter freezing

--- Dynamically_Expandable_Tests/CIFARsetup.py
+++ Dynamically_Expandable_Tests/CIFARsetup.py
@@ -120,9 +120,9 @@ class CIFAR10_Setup:
 
                 
                 for name, params in zip(freeze_name, freeze_param):
-                    print(name + ' before:', operator.attrgetter(name + '.grad')(self.net))
+                    print(name + params + ' before:', operator.attrgetter(name + '.grad')(self.net))
                     operator.attrgetter(name + '.grad')(self.net)[params, :, :] = 0
-                    print(name + ' after:', operator.attrgetter(name + '.grad')(self.net)) 
+                    print(name + params +  ' after:', operator.attrgetter(name + '.grad')(self.net)) 
                 
                 '''
                 print(operator.attrgetter(freeze_name + '.grad')(self.net))
